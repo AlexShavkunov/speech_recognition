76ffa97f7aadc1e720e306c536b3206c2f17f9ee
Alex Shavkunov <AleeexShavkunov@gmail.com>
2020-12-03 05:04:15 +0500

    Update speech_recognition.dart

--- lib/speech_recognition.dart
+++ lib/speech_recognition.dart
@@ -3,19 +3,18 @@ import 'dart:async';
 import 'dart:ui';
 import 'package:flutter/services.dart';
 
-typedef void AvailabilityHandler(bool result);
-typedef void StringResultHandler(String text);
+typedef AvailabilityHandler = void Function(bool result);
+typedef StringResultHandler = void Function(String text);
 
 /// the channel to control the speech recognition
-class SpeechRecognition {
-  static const MethodChannel _channel =
-      const MethodChannel('speech_recognition');
+class CustomSpeechRecognition {
+  static const MethodChannel _channel = MethodChannel('speech_recognition');
 
-  static final SpeechRecognition _speech = new SpeechRecognition._internal();
+  static final CustomSpeechRecognition _speech = CustomSpeechRecognition._internal();
 
-  factory SpeechRecognition() => _speech;
+  factory CustomSpeechRecognition() => _speech;
 
-  SpeechRecognition._internal() {
+  CustomSpeechRecognition._internal() {
     _channel.setMethodCallHandler(_platformCallHandler);
   }
 
@@ -27,20 +26,14 @@ class SpeechRecognition {
   VoidCallback recognitionStartedHandler;
 
   StringResultHandler recognitionCompleteHandler;
-  
+
   VoidCallback errorHandler;
 
-  /// ask for speech  recognizer permission
-  Future activate() => _channel.invokeMethod("speech.activate");
-
   /// start listening
-  Future listen({String locale}) =>
-      _channel.invokeMethod("speech.listen", locale);
+  Future listen({String locale}) => _channel.invokeMethod("speech.listen", locale);
 
-  /// cancel speech
   Future cancel() => _channel.invokeMethod("speech.cancel");
-  
-  /// stop listening
+
   Future stop() => _channel.invokeMethod("speech.stop");
 
   Future _platformCallHandler(MethodCall call) async {
@@ -65,28 +58,7 @@ class SpeechRecognition {
         errorHandler();
         break;
       default:
-        print('Unknowm method ${call.method} ');
+        print('Unknown method ${call.method}');
     }
   }
-
-  // define a method to handle availability / permission result
-  void setAvailabilityHandler(AvailabilityHandler handler) =>
-      availabilityHandler = handler;
-
-  // define a method to handle recognition result
-  void setRecognitionResultHandler(StringResultHandler handler) =>
-      recognitionResultHandler = handler;
-
-  // define a method to handle native call
-  void setRecognitionStartedHandler(VoidCallback handler) =>
-      recognitionStartedHandler = handler;
-
-  // define a method to handle native call
-  void setRecognitionCompleteHandler(StringResultHandler handler) =>
-      recognitionCompleteHandler = handler;
-
-  void setCurrentLocaleHandler(StringResultHandler handler) =>
-      currentLocaleHandler = handler;
-  
-  void setErrorHandler(VoidCallback handler) => errorHandler = handler;
 }
