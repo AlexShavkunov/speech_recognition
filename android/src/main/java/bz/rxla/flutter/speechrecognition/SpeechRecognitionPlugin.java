14f86207b63f6d4ff6d362bf6f9313922a89b3af
Alex Shavkunov <AleeexShavkunov@gmail.com>
2020-12-03 14:36:53 +0500

    Update SpeechRecognitionPlugin.java

--- android/src/main/java/bz/rxla/flutter/speechrecognition/SpeechRecognitionPlugin.java
+++ android/src/main/java/bz/rxla/flutter/speechrecognition/SpeechRecognitionPlugin.java
@@ -118,7 +118,6 @@ public class SpeechRecognitionPlugin implements MethodCallHandler, RecognitionLi
 
     @Override
     public void onError(int error) {
-        Log.d(LOG_TAG, "onError : " + error);
         speechChannel.invokeMethod("speech.onSpeechAvailability", false);
         speechChannel.invokeMethod("speech.onError", error);
     }
